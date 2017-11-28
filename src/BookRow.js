67da0d8f723d8ed9d270d6da91aad40012b5f846
Chad Looker <chad.looker@gmail.com>
2017-11-28 12:59:06 -0500

    further modularized app by creating a book component

--- src/BookRow.js
+++ src/BookRow.js
@@ -1,65 +1,36 @@
 import React from 'react';
+import Book from './Book';
 import './App.css';
 
 const BookRow = function(props) {
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>{props.myReads}</h1>
-        </div>
-        <div className="list-books-content">
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">{props.shelfTitle}</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {props.shelf.map(book => {
-                  return (
-                    <li key={book.id}>
-                      <div className="book">
-                        <div className="book-top">
-                          <div className="book-cover"
-                            style={{
-                              width: 128,
-                              height: 193,
-                              backgroundImage:
-                                (`url(
-                                    ${(book.imageLinks && book.imageLinks.thumbnail) ||
-                                    ("http://via.placeholder.com/128x193")}
-                                  )`
-                                )
-                            }}>
-                          </div>
-                          <div className="book-shelf-changer">
-                            <select
-                              onChange={props.changeBookshelf}
-                              value={book.selectedValue || " "}
-                              data={book.id}
-                            >
-                              <option value="none" disabled>Move to...</option>
-                              {props.displaySearch && <option value=" "></option>}
-                              <option value="currentlyReading">Currently Reading</option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              {book.shelf && <option value="none">None</option>}
-                            </select>
-                          </div>
-                        </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">
-                          {book.authors && book.authors.map((author)=> {
-                            return (<div key={author + Math.random()}>{author}</div>)
-                            })}
-                        </div>
-                      </div>
-                    </li>
-                  )
-                })}
-              </ol>
-            </div>
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>{props.myReads}</h1>
+      </div>
+      <div className="list-books-content">
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">
+            {props.shelfTitle}
+          </h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {props.shelf.map(book=> {
+                return (
+                  <li key={book.id}>
+                    <Book
+                      book={book}
+                      changeBookshelf={props.changeBookshelf}
+                    />
+                  </li>
+                )
+              })}
+            </ol>
           </div>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default BookRow;

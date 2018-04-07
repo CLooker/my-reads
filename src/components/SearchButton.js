09f816fc9ecfedcf992b6461861737fea19545c9
CLooker <chad.looker@gmail.com>
2018-04-07 17:19:18 -0400

    fixed url issue on search button click

--- src/components/SearchButton.js
+++ src/components/SearchButton.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const SearchButton = ({ resetSearch, ...location }) => (
+const SearchButton = ({ resetSearch, ...routeProps }) => (
   <div className="open-search">
-    <Link onClick={resetSearch} to="my-reads/search">
+    <Link onClick={resetSearch} to={`${routeProps.location.pathname}/search`}>
       Add a book
     </Link>
   </div>

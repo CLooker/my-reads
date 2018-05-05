8cb0d96b0cf9dd5631233f87af4fbf35e0490348
CLooker <chad.looker@gmail.com>
2018-05-05 12:28:27 -0400

    remove commented out code

--- src/utils/searchViewDiffer.js
+++ src/utils/searchViewDiffer.js
@@ -3,7 +3,9 @@ export const searchResultsDiffer = (searchResults, kvs) =>
     book =>
       kvs[book.id]
         ? kvs[book.id]
-        : book.shelf ? { ...book, shelf: false } : book
+        : book.shelf
+          ? { ...book, shelf: false }
+          : book
   );
 
 const searchViewDiffer = (nextProps, prevState) => {
@@ -15,26 +17,5 @@ const searchViewDiffer = (nextProps, prevState) => {
     )
   };
 };
-
-// export const returnFormattedBook = book => ({ ...book, shelf: false });
-
-// export const bookShelfChecker = book =>
-//   book.shelf ? returnFormattedBook(book) : book;
-
-// export const bookRecordExists = (kvs, book) => (kvs[book.id] ? true : false);
-
-// export const searchResultsDiffer = (searchResults, kvs) =>
-//   searchResults.map(
-//     book =>
-//       bookRecordExists(kvs, book) ? kvs[book.id] : bookShelfChecker(book)
-//   );
-
-// export const searchViewDiffer = (nextProps, prevState) => ({
-//   query: prevState.query,
-//   searchResults: searchResultsDiffer(
-//     prevState.searchResults,
-//     nextProps.shelfKeyValueStore
-//   )
-// });
 
 export default searchViewDiffer;
